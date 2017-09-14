ba1a70e5aa13cdff81d092db9e4363e7734899d4
Wayne <wayne@gamesmartltd.com>
2017-09-14 17:21:08 +0800

    add geom

--- canvasjs/Main.js
+++ canvasjs/Main.js
@@ -13,5 +13,65 @@ function gameLoad(){
 }
 
 function onAssetsAllLoaded(){
-    trace( "wahaha" );
+    trace( "start canvas game" );
+    new bond();
+}
+
+function bond(){//game main class
+    this.tamp = SuperBond; this.tamp(); delete this.tamp;
+
+    this.lotteryBalls;
+    //this.rewardBar;
+    //this.cards;
+    //this.bottomBar;
+    //this.gameBalls;
+
+    this.needClearCard = true;
+    this.isAuto = false;
+
+    this.initialize = function(){
+        this.lotteryBalls =  new LotteryBall;
+        addChild( this.lotteryBalls.entity );
+        //this.rewardBar = new RewardBar;
+        //addChild( this.rewardBar.entity );
+        //
+        //addCards();
+        //addBottom();
+    }
+
+    this.initialize();
+}
+
+function SuperBond(){
+    this.layer1;
+    this.setStage = function(){
+        var gameStage = new Stage("main");
+        $dom("main").className = "canvasDiv";
+        this.layer1 = new Layer( 1024, 768, 1000/60 );
+        this.layer1.align = "center";
+        this.layer1.entity.className = "layerDiv";
+        gameStage.addChild( this.layer1 );
+        window.onresize = this.onResize.bind(this);
+        this.onResize();
+    }
+    this.onResize = function(){
+        var wd = $dom("main").offsetWidth;
+        var ht = $dom("main").offsetHeight;
+        var scale = Math.min( wd / 1024, ht / 768 );
+        this.layer1.width = 1024 * scale;
+        this.layer1.height = 768 * scale;
+    }
+    this.setStage();
+
+    this.addLogo = function(){
+        var logoTexture = Assets.assets().logoIcon;
+        var rect = { x:0,y:0,width:logoTexture.texture.width,height:logoTexture.texture.height };
+        var logo = new Bitmap( new BitmapData( logoTexture, rect ) );
+        logo.x = 880;
+        logo.y = 550;
+        logo.smoothing = true;
+        logo.scaleX = logo.scaleY = 0.2;
+        this.layer1.addChild( logo );
+    }
+    this.addLogo();
 }
---
+++ reactjs/gamebabel.js
@@ -0,0 +1,20 @@
+var Bond = React.createClass({
+    render: function () {
+        return (<div id = "gameBg">
+
+            </div>)
+    }
+})
+function bond() {
+    ReactDOM.render(
+    < Bond / >,
+        document.getElementById('main')
+    );
+}
+//<Card cardId="0"/>
+//    <Card cardId="1"/>
+//    <Card cardId="2"/>
+//    <Card cardId="3"/>
+//    <LotteryBall/>
+//    <BottomBar/>
+//    <GameBallsLayer/>
